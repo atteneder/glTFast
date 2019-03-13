c8fce9b7571c62152821f5d567ce2aada4c231ea
Andreas Atteneder <andreas.atteneder@gmail.com>
2019-03-13 23:34:29 +0100

    Indents to spaces

--- Assets/GLTFast/DefaultMaterialGenerator.cs
+++ Assets/GLTFast/DefaultMaterialGenerator.cs
@@ -24,9 +24,9 @@ namespace GLTFast {
             material.name = gltfMaterial.name;
 
             if(gltfMaterial.pbrMetallicRoughness!=null) {
-				material.color = gltfMaterial.pbrMetallicRoughness.baseColor;
-				material.SetFloat(StandardShaderHelper.metallicPropId, gltfMaterial.pbrMetallicRoughness.metallicFactor );
-				material.SetFloat(StandardShaderHelper.glossinessPropId, 1-gltfMaterial.pbrMetallicRoughness.roughnessFactor );
+                material.color = gltfMaterial.pbrMetallicRoughness.baseColor;
+                material.SetFloat(StandardShaderHelper.metallicPropId, gltfMaterial.pbrMetallicRoughness.metallicFactor );
+                material.SetFloat(StandardShaderHelper.glossinessPropId, 1-gltfMaterial.pbrMetallicRoughness.roughnessFactor );
 
                 var mainTxt = GetTexture(gltfMaterial.pbrMetallicRoughness.baseColorTexture,textures,images);
                 if(mainTxt!=null) {
@@ -35,27 +35,27 @@ namespace GLTFast {
 
                 var metallicRoughnessTxt = GetTexture(gltfMaterial.pbrMetallicRoughness.metallicRoughnessTexture,textures,images);
                 if(metallicRoughnessTxt!=null) {
-					               
-					Profiler.BeginSample("ConvertMetallicRoughnessTexture");
-					// todo: Avoid this conversion by switching to a shader that accepts the given layout.
-					Debug.LogWarning("Convert MetallicRoughnessTexture structure to fit Unity Standard Shader (slow operation).");
-					var newmrt = new UnityEngine.Texture2D(metallicRoughnessTxt.width, metallicRoughnessTxt.height);
+                                   
+                    Profiler.BeginSample("ConvertMetallicRoughnessTexture");
+                    // todo: Avoid this conversion by switching to a shader that accepts the given layout.
+                    Debug.LogWarning("Convert MetallicRoughnessTexture structure to fit Unity Standard Shader (slow operation).");
+                    var newmrt = new UnityEngine.Texture2D(metallicRoughnessTxt.width, metallicRoughnessTxt.height);
 #if DEBUG
-					newmrt.name = string.Format("{0}_metal_smooth", metallicRoughnessTxt.name);
+                    newmrt.name = string.Format("{0}_metal_smooth", metallicRoughnessTxt.name);
 #endif
-					var buf = metallicRoughnessTxt.GetPixels32();               
-					for (int i = 0; i < buf.Length;i++ ) {
-						// TODO: Reassure given space (linear) is correct (no gamma conversion needed).
-						var color = buf[i];                  
-						color.a = (byte) (255 - color.g);
-						color.r = color.g = color.b;                  
-						buf[i] = color;
-					}
-					newmrt.SetPixels32(buf);
-					newmrt.Apply();
-					Profiler.EndSample();
+                    var buf = metallicRoughnessTxt.GetPixels32();               
+                    for (int i = 0; i < buf.Length;i++ ) {
+                        // TODO: Reassure given space (linear) is correct (no gamma conversion needed).
+                        var color = buf[i];                  
+                        color.a = (byte) (255 - color.g);
+                        color.r = color.g = color.b;                  
+                        buf[i] = color;
+                    }
+                    newmrt.SetPixels32(buf);
+                    newmrt.Apply();
+                    Profiler.EndSample();
 
-					material.SetTexture( StandardShaderHelper.metallicGlossMapPropId, newmrt );
+                    material.SetTexture( StandardShaderHelper.metallicGlossMapPropId, newmrt );
                     material.EnableKeyword("_METALLICGLOSSMAP");
 
                     additionalResources.Add(newmrt);
@@ -67,34 +67,34 @@ namespace GLTFast {
                 material.SetTexture( StandardShaderHelper.bumpMapPropId, normalTxt);
                 material.EnableKeyword("_NORMALMAP");
             }
-			
+            
             var occlusionTxt = GetTexture(gltfMaterial.occlusionTexture,textures,images);
             if(occlusionTxt !=null) {
 
-				Profiler.BeginSample("ConvertOcclusionTexture");
+                Profiler.BeginSample("ConvertOcclusionTexture");
                 // todo: Avoid this conversion by switching to a shader that accepts the given layout.
-				Debug.LogWarning("Convert OcclusionTexture structure to fit Unity Standard Shader (slow operation).");
-				var newOcclusionTxt = new UnityEngine.Texture2D(occlusionTxt.width, occlusionTxt.height);
+                Debug.LogWarning("Convert OcclusionTexture structure to fit Unity Standard Shader (slow operation).");
+                var newOcclusionTxt = new UnityEngine.Texture2D(occlusionTxt.width, occlusionTxt.height);
 #if DEBUG
-				newOcclusionTxt.name = string.Format("{0}_occlusion", occlusionTxt.name);
+                newOcclusionTxt.name = string.Format("{0}_occlusion", occlusionTxt.name);
 #endif
-				var buf = occlusionTxt.GetPixels32();
+                var buf = occlusionTxt.GetPixels32();
                 for (int i = 0; i < buf.Length; i++)
                 {
                     var color = buf[i];
-					color.g = color.b = color.r;
-					color.a = 1;
+                    color.g = color.b = color.r;
+                    color.a = 1;
                     buf[i] = color;
                 }
                 newOcclusionTxt.SetPixels32(buf);
                 newOcclusionTxt.Apply();
                 Profiler.EndSample();
 
-				material.SetTexture( StandardShaderHelper.occlusionMapPropId, newOcclusionTxt );
+                material.SetTexture( StandardShaderHelper.occlusionMapPropId, newOcclusionTxt );
 
                 additionalResources.Add(newOcclusionTxt);
             }
-			
+            
             var emmissiveTxt = GetTexture(gltfMaterial.emissiveTexture,textures,images);
             if(emmissiveTxt!=null) {
                 material.SetTexture( StandardShaderHelper.emissionMapPropId, emmissiveTxt);
@@ -103,18 +103,18 @@ namespace GLTFast {
             
             if(gltfMaterial.alphaModeEnum == AlphaMode.MASK) {
                 material.SetFloat(StandardShaderHelper.cutoffPropId, gltfMaterial.alphaCutoff);
-				StandardShaderHelper.SetAlphaModeMask( material );
+                StandardShaderHelper.SetAlphaModeMask( material );
             } else if(gltfMaterial.alphaModeEnum == AlphaMode.BLEND) {
                 StandardShaderHelper.SetAlphaModeBlend( material );
             }
 
             if(gltfMaterial.emissive != Color.black) {
-				material.SetColor("_EmissionColor", gltfMaterial.emissive);
-				material.EnableKeyword("_EMISSION");
+                material.SetColor("_EmissionColor", gltfMaterial.emissive);
+                material.EnableKeyword("_EMISSION");
             }
 
             if(gltfMaterial.doubleSided) {
-				Debug.LogWarning("Double sided shading is not supported!");
+                Debug.LogWarning("Double sided shading is not supported!");
             }
 
             return material;
