53c42653144c28e8ae5deb52b6c0e8d56759af8e
Andreas Atteneder <andreas.atteneder@gmail.com>
2019-12-03 00:51:50 +0100

    Fix: set proper defaults for texture transform.

--- Runtime/Scripts/Schema/TextureTransform.cs
+++ Runtime/Scripts/Schema/TextureTransform.cs
@@ -6,18 +6,18 @@
         /// <summary>
         /// The offset of the UV coordinate origin as a factor of the texture dimensions.
         /// </summary>
-        public float[] offset; // 	array[2] 	[0.0, 0.0] 	
-        
+        public float[] offset = {0,0};
+
         /// <summary>
         /// Rotate the UVs by this many radians counter-clockwise around the origin. This is equivalent to a similar rotation of the image clockwise.
         /// </summary>
         public float rotation = 0;
-        
+
         /// <summary>
         /// The scale factor applied to the components of the UV coordinates.
         /// </summary>
-        public float[] scale; // 	array[2] 	[1.0, 1.0] 	
-        
+        public float[] scale = {1,1};
+
         /// <summary>
         /// Overrides the textureInfo texCoord value if supplied, and if this extension is supported.
         /// </summary>
