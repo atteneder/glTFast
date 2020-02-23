9996cf4c7b68766f1c0a36a6b169abf97489cced
Andreas Atteneder <andreas.atteneder@gmail.com>
2020-02-23 19:26:45 +0100

    Fix: Implementation of levels is inconsistent and JSON decoder complains. We don't need it anyways (yet).

--- Runtime/Scripts/Schema/ImageKtx2.cs
+++ Runtime/Scripts/Schema/ImageKtx2.cs
@@ -5,7 +5,8 @@ namespace GLTFast.Schema {
     [System.Serializable]
     public class ImageKtx2 {
         public uint faceCount;
-        public BufferSlice[] levels;
+        // public BufferSlice levels; // Obsolete KHR_texture_cttf way
+        // public BufferSlice[] levels;
         public uint pixelHeight;
         public uint pixelWidth;
         public uint supercompressionScheme;
