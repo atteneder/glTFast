1f2c9106d382e42f2900ebe26c29b81aa4cf9f44
Andreas Atteneder <andreas.atteneder@gmail.com>
2020-03-06 22:36:50 +0100

    WIP: Use new mesh API.

--- Runtime/Scripts/PrimitiveCreateContext.cs
+++ Runtime/Scripts/PrimitiveCreateContext.cs
@@ -14,6 +14,12 @@
 //
 
 using UnityEngine;
+#if !UNITY_2019_3_OR_NEWER
+#define LEGACY_MESH
+#endif
+
+using UnityEngine;
+using UnityEngine.Rendering;
 using Unity.Jobs;
 using System.Runtime.InteropServices;
 using UnityEngine.Profiling;
@@ -55,14 +61,20 @@ namespace GLTFast {
             jobHandle.Complete();
             Profiler.EndSample();
             var msh = new UnityEngine.Mesh();
+            msh.name = mesh.name;
+
+            bool calculateNormals = needsNormals && ( topology==MeshTopology.Triangles || topology==MeshTopology.Quads );
+            bool calculateTangents = needsTangents && uvs0!=null && (topology==MeshTopology.Triangles || topology==MeshTopology.Quads);
+
+#if LEGACY_MESH
             if( positions.Length > 65536 ) {
 #if UNITY_2017_3_OR_NEWER
                 msh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
 #else
                 throw new System.Exception("Meshes with more than 65536 vertices are only supported from Unity 2017.3 onwards.");
-#endif
+#endif // UNITY_2017_3_OR_NEWER
             }
-            msh.name = mesh.name;
+
             Profiler.BeginSample("SetVertices");
             msh.vertices = positions;
             Profiler.EndSample();
@@ -71,6 +83,28 @@ namespace GLTFast {
             msh.subMeshCount = indices.Length;
             for (int i = 0; i < indices.Length; i++) {
                 msh.SetIndices(indices[i],topology,i);
+            }
+            Profiler.EndSample();
+
+            Profiler.BeginSample("SetNormals");
+            if(normals!=null) {
+                msh.normals = normals;
+            } else
+            if(calculateNormals) {
+                Profiler.BeginSample("RecalculateNormals");
+                msh.RecalculateNormals();
+                Profiler.EndSample();
+            }
+            Profiler.EndSample();
+
+            Profiler.BeginSample("SetTangents");
+            if(tangents!=null) {
+                msh.tangents = tangents;
+            } else
+            if(calculateTangents) {
+                Profiler.BeginSample("RecalculateTangents");
+                msh.RecalculateTangents();
+                Profiler.EndSample();
             }
             Profiler.EndSample();
 
@@ -83,16 +117,6 @@ namespace GLTFast {
             }
             Profiler.EndSample();
 
-            Profiler.BeginSample("SetNormals");
-            if(normals!=null) {
-                msh.normals = normals;
-            } else
-            if( needsNormals && ( topology==MeshTopology.Triangles || topology==MeshTopology.Quads ) ) {
-                Profiler.BeginSample("RecalculateNormals");
-                msh.RecalculateNormals();
-                Profiler.EndSample();
-            }
-            Profiler.EndSample();
             Profiler.BeginSample("SetColor");
             if (colors!=null) {
                 msh.colors = colors;
@@ -100,20 +124,118 @@ namespace GLTFast {
                 msh.colors32 = colors32;
             }
             Profiler.EndSample();
-            Profiler.BeginSample("SetTangents");
+
+            Profiler.BeginSample("UploadMeshData");
+            msh.UploadMeshData(true);
+            Profiler.EndSample();
+
+#else // #if LEGACY_MESH
+
+            Profiler.BeginSample("PrepareMesh");
+            MeshUpdateFlags flags = (MeshUpdateFlags)~0;
+            int vadLen = 1;
+            if(uvs0!=null) vadLen++;
+            if(uvs1!=null) vadLen++;
+            if(normals!=null || calculateNormals)
+                vadLen++;
+            if(tangents!=null || calculateTangents)
+                vadLen++;
+
+            var vad = new VertexAttributeDescriptor[vadLen];
+            var vadCount = 0;
+            vad[vadCount] = new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, 3, vadCount);
+            vadCount++;
+            if(uvs0!=null) {
+                vad[vadCount] = new VertexAttributeDescriptor(VertexAttribute.TexCoord0, VertexAttributeFormat.Float32, 2, vadCount);
+                vadCount++;
+            }
+            if(uvs1!=null) {
+                vad[vadCount] = new VertexAttributeDescriptor(VertexAttribute.TexCoord1, VertexAttributeFormat.Float32, 2, vadCount);
+                vadCount++;
+            }
+            if(normals!=null || calculateNormals) {
+                vad[vadCount] = new VertexAttributeDescriptor(VertexAttribute.Normal, VertexAttributeFormat.Float32, 3, vadCount);
+                vadCount++;
+            }
+            if(tangents!=null || calculateTangents) {
+                vad[vadCount] = new VertexAttributeDescriptor(VertexAttribute.Tangent, VertexAttributeFormat.Float32, 4, vadCount);
+                vadCount++;
+            }
+
+            msh.SetVertexBufferParams(positions.Length,vad);
+            Profiler.EndSample();
+
+            Profiler.BeginSample("SetVertices");
+            vadCount = 0;
+            msh.SetVertexBufferData(positions,0,0,positions.Length,vadCount,flags);
+            vadCount++;
+            Profiler.EndSample();
+
+            if(uvs0!=null) {
+                Profiler.BeginSample("SetUVs0");
+                msh.SetVertexBufferData(uvs0,0,0,uvs0.Length,vadCount,flags);
+                vadCount++;
+                Profiler.EndSample();
+            }
+            if(uvs1!=null) {
+                Profiler.BeginSample("SetUVs1");
+                msh.SetVertexBufferData(uvs1,0,0,uvs1.Length,vadCount,flags);
+                vadCount++;
+                Profiler.EndSample();
+            }
+            if(normals!=null) {
+                Profiler.BeginSample("SetNormals");
+                msh.SetVertexBufferData(normals,0,0,normals.Length,vadCount,flags);
+                vadCount++;
+                Profiler.EndSample();
+            }
+
             if(tangents!=null) {
-                msh.tangents = tangents;
-            } else
-            if( needsTangents && uvs0!=null && (topology==MeshTopology.Triangles || topology==MeshTopology.Quads) ) {
+                Profiler.BeginSample("SetTangents");
+                msh.SetVertexBufferData(tangents,0,0,tangents.Length,vadCount,flags);
+                vadCount++;
+                Profiler.EndSample();
+            }
+
+            Profiler.BeginSample("SetIndices");
+            int indexCount = 0;
+            for (int i = 0; i < indices.Length; i++) {
+                indexCount += indices[i].Length;
+            }
+            msh.SetIndexBufferParams(indexCount,IndexFormat.UInt32); //TODO: UInt16 maybe?
+            msh.subMeshCount = indices.Length;
+            indexCount = 0;
+            for (int i = 0; i < indices.Length; i++) {
+                Profiler.BeginSample("SetIndexBufferData");
+                msh.SetIndexBufferData(indices[i],0,indexCount,indices[i].Length,flags);
+                Profiler.EndSample();
+                Profiler.BeginSample("SetSubMesh");
+                msh.SetSubMesh(i,new SubMeshDescriptor(indexCount,indices[i].Length,topology),flags);
+                Profiler.EndSample();
+                indexCount += indices[i].Length;
+            }
+            Profiler.EndSample();
+
+            if(normals==null && calculateNormals) {
+                Profiler.BeginSample("RecalculateNormals");
+                msh.RecalculateNormals();
+                Profiler.EndSample();
+            }
+            if(tangents==null && calculateTangents) {
                 Profiler.BeginSample("RecalculateTangents");
                 msh.RecalculateTangents();
                 Profiler.EndSample();
             }
+
+            Profiler.BeginSample("RecalculateBounds");
+            msh.RecalculateBounds(); // TODO: make optional! maybe calculate bounds in Job.
             Profiler.EndSample();
 
             Profiler.BeginSample("UploadMeshData");
             msh.UploadMeshData(true);
             Profiler.EndSample();
+
+#endif // LEGACY_MESH
 
             Profiler.BeginSample("Dispose");
             Dispose();
