2dc1564c6c5fc19b368222719c3f309bfd11ca29
Andreas Atteneder <andreas.atteneder@gmail.com>
2020-12-01 00:26:00 +0100

    fix: Shader GUI texture rotation calculation

--- Editor/GLTFastShaderGUIBase.cs
+++ Editor/GLTFastShaderGUIBase.cs
@@ -73,7 +73,7 @@ namespace GLTFast.Editor
                 var currentScaleTransform = material.GetVector(scaleTransformPropertyId);
                 float2x2 rotScale = math.mul(new float2x2(cos, sin, -sin, cos), new float2x2(newUvTransform.scale.x,0,0,newUvTransform.scale.y));
                 material.SetVector(scaleTransformPropertyId,new Vector4(rotScale.c0.x,rotScale.c1.y,currentScaleTransform.z,currentScaleTransform.w));
-                material.SetVector(rotationPropertyId,new Vector4(rotScale.c0.y,rotScale.c1.x,0,0));
+                material.SetVector(rotationPropertyId,new Vector4(rotScale.c1.x,rotScale.c0.y,0,0));
                 if (newUvTransform.rotation == 0) {
                     material.DisableKeyword(KW_UV_ROTATION);
                 } else {
@@ -94,8 +94,8 @@ namespace GLTFast.Editor
             float4 st = material.GetVector(scaleTransformPropertyId);
             float2 r = (Vector2)material.GetVector(rotationPropertyId);
 
-            uvTransform.scale.x = Mathematics.normalize(new float2(st.x,r.x), out var r1);
-            uvTransform.scale.y = Mathematics.normalize(new float2(st.y,r.y), out var r2);
+            uvTransform.scale.x = Mathematics.normalize(new float2(st.x,r.y), out var r1);
+            uvTransform.scale.y = Mathematics.normalize(new float2(st.y,r.x), out var r2);
 
             var acos = Mathf.Acos(r1.x);
             if (r2.x < 0) acos = Mathf.PI*2-acos;
