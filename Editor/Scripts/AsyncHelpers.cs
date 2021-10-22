b0829d6c24f6cea8edfe4294c5465dec8789b3d7
Andreas Atteneder <andreas.atteneder@gmail.com>
2021-10-22 18:54:10 +0200

    fix: Corrected source

--- Editor/Scripts/AsyncHelpers.cs
+++ Editor/Scripts/AsyncHelpers.cs
@@ -1,4 +1,4 @@
-﻿// Source: https://stackoverflow.com/questions/5095183/how-would-i-run-an-async-taskt-method-synchronously/5097066#5097066
+﻿// Source: https://social.msdn.microsoft.com/Forums/en-US/163ef755-ff7b-4ea5-b226-bbe8ef5f4796/is-there-a-pattern-for-calling-an-async-method-synchronously?forum=async
 
 using System;
 using System.Threading;
