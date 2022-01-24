3b3512d7337a26f88f1639be3a00ca78047111e2
Andreas Atteneder <andreas.atteneder@gmail.com>
2022-01-24 17:28:32 +0100

    Release 4.5.0

--- Runtime/Scripts/Export/Constants.cs
+++ Runtime/Scripts/Export/Constants.cs
@@ -17,6 +17,6 @@ using UnityEngine;
 
 namespace GLTFast.Export {
     static class Constants {
-        public const string version = "4.4.11";   
+        public const string version = "4.5.0";   
     }
 }
