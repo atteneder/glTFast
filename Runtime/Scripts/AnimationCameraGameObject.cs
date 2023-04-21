b7e43e6b2b5cf3657f7430e0ff1f55e16473a1b3
Mike Nisbet <nisbet.mr@gmail.com>
2023-04-21 16:38:52 +0100

    namespace

--- Runtime/Scripts/AnimationCameraGameObject.cs
+++ Runtime/Scripts/AnimationCameraGameObject.cs
@@ -2,71 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationCameraGameObject : MonoBehaviour {
-    public Camera targetCamera;
-    public bool orthographic;
-    public float localScale;
+namespace GLTFast {
+    public class AnimationCameraGameObject : MonoBehaviour {
+        public Camera targetCamera;
+        public bool orthographic;
+        public float localScale;
 
-    public float nearClipPlane;
-    float nearClipPlaneOld;
-    public float farClipPlane;
-    float farClipPlaneOld;
+        public float nearClipPlane;
+        float nearClipPlaneOld;
+        public float farClipPlane;
+        float farClipPlaneOld;
 
-    public float xMag;
-    public float yMag;
-    float xMagOld;
-    float yMagOld;
+        public float xMag;
+        public float yMag;
+        float xMagOld;
+        float yMagOld;
 
-    public float fov;
-    float fovOld;
+        public float fov;
+        float fovOld;
 
-    void OnEnable() {
-        if (targetCamera == null) {
-            Debug.LogError("Target camera not set on animated camera!");
-        }
-    }
-
-    void LateUpdate() {
-        bool orthoChanged = false;
-        if(farClipPlane != farClipPlaneOld) {
-            if(orthographic) {
-                orthoChanged = true;
-                farClipPlane = farClipPlane >= 0 ? farClipPlane : float.MaxValue;
+        void OnEnable() {
+            if (targetCamera == null) {
+                Debug.LogError("Target camera not set on animated camera!");
             }
-            targetCamera.farClipPlane = localScale * farClipPlane;
-            farClipPlaneOld = farClipPlane;
         }
 
-        if(nearClipPlane != nearClipPlaneOld) {
-            targetCamera.nearClipPlane = localScale * nearClipPlane;
-            nearClipPlaneOld = nearClipPlane;
-            orthoChanged = true;
-        }
+        void LateUpdate() {
+            bool orthoChanged = false;
+            if(farClipPlane != farClipPlaneOld) {
+                if(orthographic) {
+                    orthoChanged = true;
+                    farClipPlane = farClipPlane >= 0 ? farClipPlane : float.MaxValue;
+                }
+                targetCamera.farClipPlane = localScale * farClipPlane;
+                farClipPlaneOld = farClipPlane;
+            }
 
-        if(xMag != xMagOld) {
-            xMagOld = xMag;
-            orthoChanged = true;
-        }
+            if(nearClipPlane != nearClipPlaneOld) {
+                targetCamera.nearClipPlane = localScale * nearClipPlane;
+                nearClipPlaneOld = nearClipPlane;
+                orthoChanged = true;
+            }
 
-        if(yMag != yMagOld) {
-            yMagOld = yMag;
-            orthoChanged = true;
-        }
+            if(xMag != xMagOld) {
+                xMagOld = xMag;
+                orthoChanged = true;
+            }
 
-        if(fov != fovOld) {
-            targetCamera.fieldOfView = fov * Mathf.Rad2Deg;
-            fovOld = fov;
-        }
+            if(yMag != yMagOld) {
+                yMagOld = yMag;
+                orthoChanged = true;
+            }
 
-        if(orthographic && orthoChanged) {
-            targetCamera.projectionMatrix = Matrix4x4.Ortho(
-                -xMag,
-                xMag,
-                -yMag,
-                yMag,
-                nearClipPlane,
-                farClipPlane
-            );
+            if(fov != fovOld) {
+                targetCamera.fieldOfView = fov * Mathf.Rad2Deg;
+                fovOld = fov;
+            }
+
+            if(orthographic && orthoChanged) {
+                targetCamera.projectionMatrix = Matrix4x4.Ortho(
+                    -xMag,
+                    xMag,
+                    -yMag,
+                    yMag,
+                    nearClipPlane,
+                    farClipPlane
+                );
+            }
         }
     }
 }
