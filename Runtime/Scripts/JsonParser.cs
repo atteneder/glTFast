c3d0e969263ecfd26af14bebca13a00dd578d5c9
Andreas Atteneder <andreas.atteneder@unity3d.com>
2023-08-28 17:08:16 +0200

    fix: Use Newtonsoft JSON as default parser when scripting define `GLTFAST_USE_NEWTONSOFT_JSON` is set. (#33)
    Co-authored-by: Andreas Atteneder <andreas.atteneder@gmail.com>

--- Runtime/Scripts/JsonParser.cs
+++ Runtime/Scripts/JsonParser.cs
@@ -1,6 +1,12 @@
 // SPDX-FileCopyrightText: 2023 Unity Technologies and the glTFast authors
 // SPDX-License-Identifier: Apache-2.0
 
+#if NEWTONSOFT_JSON && GLTFAST_USE_NEWTONSOFT_JSON
+#define JSON_NEWTONSOFT
+#else
+#define JSON_UTILITY
+#endif
+
 using System;
 
 namespace GLTFast
@@ -14,7 +20,11 @@ namespace GLTFast
         internal static Root ParseJson<T>(string json) where T : Root
         {
             if (k_DefaultJsonParser is null)
+#if JSON_NEWTONSOFT
+                SetDefaultParser<GltfNewtonsoftJsonParser>();
+#else
                 SetDefaultParser<GltfJsonUtilityParser>();
+#endif
 
             if (k_DefaultJsonParser is null)
                 throw new InvalidOperationException(
